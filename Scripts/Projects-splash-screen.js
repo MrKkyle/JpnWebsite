2d674585ec8db36897e35be0aaf55e1858e0b01a
MrKkyle <Kyle.apocrypha@gmail.com>
2023-01-06 10:05:27 +0200

    update 1.0

---
+++ Scripts/Projects-splash-screen.js
@@ -0,0 +1,18 @@
+
+const splash = document.querySelector(".splash");
+
+window.addEventListener("load", event =>
+{
+    setTimeout(() =>
+    {
+        splash.classList.add("display-none");
+    }, 2000);
+})
+
+
+
+
+
+
+
+
---
+++ Scripts/Video-container.js
@@ -0,0 +1,15 @@
+vid1 = document.getElementById("vid1");
+vid2 = document.getElementById("vid2");
+vid3 = document.getElementById("vid3");
+vid4 = document.getElementById("vid4");
+
+vidPlayer = document.getElementById("")
+
+
+
+
+
+
+
+
+
