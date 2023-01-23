74672815fcca5275992aff59f46a009a142eed02
MrKkyle <Kyle.apocrypha@gmail.com>
2023-01-23 11:24:50 +0200

    update

--- Scripts/Video-container.js
+++ Scripts/Video-container.js
@@ -2,6 +2,8 @@ vid1 = document.getElementById("vid1");
 vid2 = document.getElementById("vid2");
 vid3 = document.getElementById("vid3");
 vid4 = document.getElementById("vid4");
+vid5 = document.getElementById("vid5");
+vid6 = document.getElementById("vid6");
 
 vidPlayer = document.getElementById("")
 
