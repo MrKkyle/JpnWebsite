f8a1b88e688b4fa65ac04bdb7042ab26f9d77420
MrKkyle <Kyle.apocrypha@gmail.com>
2023-02-09 12:40:17 +0200

    update

--- Scripts/Video-container.js
+++ Scripts/Video-container.js
@@ -4,14 +4,47 @@ vid3 = document.getElementById("vid3");
 vid4 = document.getElementById("vid4");
 vid5 = document.getElementById("vid5");
 vid6 = document.getElementById("vid6");
+vidPlayer = document.getElementById("vidPlayer");
 
-vidPlayer = document.getElementById("")
-
-
-
-
-
-
-
-
-
+vid1.onclick = function(event)
+{
+    if(vid1.onclick)
+    {
+        vidPlayer.src = "https://www.youtube.com/embed/SOUK-YgFi6o";
+    }
+}
+vid2.onclick = function(event)
+{
+    if(vid2.onclick)
+    {
+        vidPlayer.src = "https://www.youtube.com/embed/yK4aAYHKAjg";
+    }
+}
+vid3.onclick = function(event)
+{
+    if(vid3.onclick)
+    {
+        vidPlayer.src = "https://www.youtube.com/embed/1cncN37SLuI";
+    }
+}
+vid4.onclick = function(event)
+{
+    if(vid4.onclick)
+    {
+        vidPlayer.src = "https://www.youtube.com/embed/4flSBiC5edI";
+    }
+}
+vid5.onclick = function(event)
+{
+    if(vid5.onclick)
+    {
+        vidPlayer.src = "https://www.youtube.com/embed/pfiGoGyPsCg";
+    }
+}
+vid6.onclick = function(event)
+{
+    if(vid6.onclick)
+    {
+        vidPlayer.src = "https://www.youtube.com/embed/nTCZUZQCZ8Q";
+    }
+}
