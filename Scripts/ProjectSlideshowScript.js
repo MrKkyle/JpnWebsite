4cd0a54af63bc180f899ccde61b4675561d2c188
MrKkyle <Kyle.apocrypha@gmail.com>
2023-02-17 13:51:08 +0200

    update

--- Scripts/ProjectSlideshowScript.js
+++ Scripts/ProjectSlideshowScript.js
@@ -34,5 +34,4 @@ function showSlides(n)
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
 }
